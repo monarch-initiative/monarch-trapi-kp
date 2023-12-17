b7bc0d8f6859fe94ba1157e61570173288800038
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2023-12-16 20:51:28 -0500

    Copied in the other two PLATER files, but not-functional given missing imports to copy and customize

---
+++ mta/services/app_common.py
@@ -0,0 +1,214 @@
+"""FastAPI app."""
+import json
+from typing import Any, Dict, List
+
+from fastapi import Body, Depends, FastAPI
+from fastapi.responses import JSONResponse
+
+from mta.models.models_trapi_1_0 import (
+    Message, ReasonerRequest, CypherRequest, SimpleSpecResponse, SimpleSpecElement,
+    GraphSummaryResponse, CypherResponse, PredicatesResponse
+)
+from mta.services.util.bl_helper import BLHelper
+from mta.services.util.graph_adapter import GraphInterface
+from mta.services.util.metadata import GraphMetadata
+from mta.services.util.overlay import Overlay
+from mta.services.util.question import Question
+from mta.services.util.api_utils import get_graph_interface, \
+    get_bl_helper, construct_open_api_schema, get_example, get_graph_metadata
+
+
+APP_COMMON = FastAPI(openapi_url='/common/openapi.json', docs_url='/common/docs')
+
+
+async def cypher(
+        request: CypherRequest = Body(
+            ...,
+            example={"query": "MATCH (n) RETURN count(n)"},
+        ),
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+) -> CypherResponse:
+    """Handle cypher."""
+    request = request.dict()
+    results = await graph_interface.run_cypher(
+        request["query"],
+        return_errors=True,
+    )
+    return results
+
+
+APP_COMMON.add_api_route(
+    "/cypher",
+    cypher,
+    methods=["POST"],
+    response_model=CypherResponse,
+    summary="Run cypher query",
+    description=(
+        "Runs cypher query against the Neo4j instance, and returns an "
+        "equivalent response expected from a Neo4j HTTP endpoint "
+        "(https://neo4j.com/docs/rest-docs/current/)."
+    ),
+)
+
+
+async def overlay(
+        request: ReasonerRequest = Body(
+            ...,
+            example={"message": get_example("overlay")},
+        ),
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+) -> Message:
+    """Handle TRAPI request."""
+    overlay_class = Overlay(graph_interface)
+    return await overlay_class.overlay_support_edges(request.dict()["message"])
+
+
+APP_COMMON.add_api_route(
+    "/overlay",
+    overlay,
+    methods=["POST"],
+    response_model=Message,
+    description=(
+        "Given a ReasonerAPI graph, add support edges for any nodes linked in "
+        "result bindings."
+    ),
+    summary="Overlay results with available connections between each node.",
+    tags=["translator"]
+)
+
+
+async def metadata(
+        graph_metadata: GraphMetadata = Depends(get_graph_metadata),
+) -> Any:
+    """Handle /metadata."""
+    response = await graph_metadata.get_metadata()
+    return response
+
+
+APP_COMMON.add_api_route(
+    "/metadata",
+    metadata,
+    methods=["GET"],
+    response_model=Any,
+    summary="Metadata about the knowledge graph.",
+    description="Returns JSON with metadata about the data sources in this knowledge graph.",
+)
+
+
+async def one_hop(
+        source_type: str,
+        target_type: str,
+        curie: str,
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+) -> List[Dict]:
+    """Handle one-hop."""
+    return await graph_interface.get_single_hops(
+        source_type,
+        target_type,
+        curie,
+    )
+
+
+APP_COMMON.add_api_route(
+    "/{source_type}/{target_type}/{curie}",
+    one_hop,
+    methods=["GET"],
+    response_model=List,
+    summary=(
+        "Get one hop results from source type to target type. "
+        "Note: Please GET /predicates to determine what target goes "
+        "with a source"
+    ),
+    description=(
+        "Returns one hop paths from `source_node_type`  with `curie` "
+        "to `target_node_type`."
+    ),
+)
+
+
+async def node(
+        node_type: str,
+        curie: str,
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+) -> List[List[Dict]]:
+    """Handle node lookup."""
+    return await graph_interface.get_node(
+        node_type,
+        curie,
+    )
+
+
+APP_COMMON.add_api_route(
+    "/{node_type}/{curie}",
+    node,
+    methods=["GET"],
+    response_model=List,
+    summary="Find `node` by `curie`",
+    description="Returns `node` matching `curie`.",
+)
+
+
+async def simple_spec(
+        source: str = None,
+        target: str = None,
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+        bl_helper: BLHelper = Depends(get_bl_helper),
+) -> SimpleSpecResponse:
+    """Handle simple spec."""
+    source_id = source
+    target_id = target
+    if source_id or target_id:
+        minischema = []
+        mini_schema_raw = await graph_interface.get_mini_schema(
+            source_id,
+            target_id,
+        )
+        for row in mini_schema_raw:
+            source_labels = await bl_helper.get_most_specific_concept(
+                row['source_label']
+            )
+            target_labels = await bl_helper.get_most_specific_concept(
+                row['target_label']
+            )
+            for source_type in source_labels:
+                for target_type in target_labels:
+                    minischema.append((
+                        source_type,
+                        row['predicate'],
+                        target_type,
+                    ))
+        minischema = list(set(minischema))  # remove dups
+        return list(map(lambda x: SimpleSpecElement(**{
+                'source_type': x[0],
+                'target_type': x[2],
+                'edge_type': x[1],
+            }), minischema))
+    else:
+        schema = graph_interface.get_schema()
+        reformatted_schema = []
+        for source_type in schema:
+            for target_type in schema[source_type]:
+                for edge in schema[source_type][target_type]:
+                    reformatted_schema.append(SimpleSpecElement(**{
+                        'source_type': source_type,
+                        'target_type': target_type,
+                        'edge_type': edge
+                    }))
+        return reformatted_schema
+
+
+APP_COMMON.add_api_route(
+    "/simple_spec",
+    simple_spec,
+    methods=["GET"],
+    response_model=SimpleSpecResponse,
+    summary="Get one-hop connection schema",
+    description=(
+        "Returns a list of available predicates when choosing a single source "
+        "or target curie. Calling this endpoint with no query parameters will "
+        "return all possible hops for all types."
+    ),
+)
+
+
+APP_COMMON.openapi_schema = construct_open_api_schema(app=APP_COMMON, trapi_version="N/A")
---
+++ mta/services/app_trapi_1_4.py
@@ -0,0 +1,99 @@
+"""FastAPI app."""
+
+from fastapi import Body, Depends, FastAPI, Response, status
+from reasoner_transpiler.exceptions import InvalidPredicateError
+from mta.models.models_trapi_1_1 import (MetaKnowledgeGraph, Message, ReasonerRequest)
+from mta.models.shared import SRITestData
+
+from mta.services.util.graph_adapter import GraphInterface
+from mta.services.util.metadata import GraphMetadata
+from mta.services.util.question import Question
+from mta.services.util.overlay import Overlay
+from mta.services.util.api_utils import get_graph_interface, get_graph_metadata, construct_open_api_schema, get_example
+
+# Mount open api at /1.4/openapi.json
+APP_TRAPI_1_4 = FastAPI(openapi_url="/openapi.json", docs_url="/docs", root_path='/1.4')
+
+
+async def get_meta_knowledge_graph(
+        graph_metadata: GraphMetadata = Depends(get_graph_metadata),
+) -> MetaKnowledgeGraph:
+    """Handle /meta_knowledge_graph."""
+    meta_kg = await graph_metadata.get_meta_kg()
+    return meta_kg
+
+
+async def get_sri_testing_data(
+        graph_metadata: GraphMetadata = Depends(get_graph_metadata),
+) -> SRITestData:
+    """Handle /sri_testing_data."""
+    sri_test_data = await graph_metadata.get_sri_testing_data()
+    return sri_test_data
+
+
+async def reasoner_api(
+        response: Response,
+        request: ReasonerRequest = Body(
+            ...,
+            # Works for now but in deployment would be replaced by a mount, specific to backend dataset
+            example=get_example("reasoner-trapi-1.3"),
+        ),
+        graph_interface: GraphInterface = Depends(get_graph_interface),
+):
+    """Handle TRAPI request."""
+    request_json = request.dict(by_alias=True)
+    # default workflow
+    workflow = request_json.get('workflow') or [{"id": "lookup"}]
+    workflows = {wkfl['id']: wkfl for wkfl in workflow}
+    if 'lookup' in workflows:
+        question = Question(request_json["message"])
+        try:
+            response_message = await question.answer(graph_interface)
+            request_json.update({'message': response_message, 'workflow': workflow})
+        except InvalidPredicateError as e:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            request_json["description"] = str(e)
+            return request_json
+    elif 'overlay_connect_knodes' in workflows:
+        overlay = Overlay(graph_interface=graph_interface)
+        response_message = await overlay.connect_k_nodes(request_json['message'])
+        request_json.update({'message': response_message, 'workflow': workflow})
+    elif 'annotate_nodes' in workflows:
+        overlay = Overlay(graph_interface=graph_interface)
+        response_message = await overlay.annotate_node(request_json['message'])
+        request_json.update({'message': response_message, 'workflow': workflow})
+    return request_json
+
+
+APP_TRAPI_1_4.add_api_route(
+    "/meta_knowledge_graph",
+    get_meta_knowledge_graph,
+    methods=["GET"],
+    response_model=MetaKnowledgeGraph,
+    summary="Meta knowledge graph representation of this TRAPI web service.",
+    description="Returns meta knowledge graph representation of this TRAPI web service.",
+    tags=["trapi"]
+)
+
+APP_TRAPI_1_4.add_api_route(
+    "/sri_testing_data",
+    get_sri_testing_data,
+    methods=["GET"],
+    response_model=SRITestData,
+    response_model_exclude_none=True,
+    summary="Test data for usage by the SRI Testing Harness.",
+    description="Returns a list of edges that are representative examples of the knowledge graph.",
+    tags=["trapi"]
+)
+
+APP_TRAPI_1_4.add_api_route(
+    "/query",
+    reasoner_api,
+    methods=["POST"],
+    response_model=ReasonerRequest,
+    summary="Query reasoner via one of several inputs.",
+    description="",
+    tags=["trapi"]
+)
+
+APP_TRAPI_1_4.openapi_schema = construct_open_api_schema(app=APP_TRAPI_1_4, trapi_version="1.4.0", prefix='/1.4')
