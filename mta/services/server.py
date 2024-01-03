07fd74ffb1cce45c3ba277cef6ffbcb3d6931f9b
richardbruskiewich <richard.bruskiewich@delphinai.com>
2024-01-03 15:17:24 -0800

    plater erasure

--- mta/services/server.py
+++ mta/services/server.py
@@ -11,10 +11,9 @@ from mta.services.app_common import APP_COMMON
 from mta.services.app_trapi_1_4 import APP_TRAPI_1_4
 from mta.services.util.api_utils import construct_open_api_schema
 
-TITLE = config.get('MTA_TITLE', 'Plater API')
+TITLE = config.get('MTA_TITLE', 'Monarch Translator ARA')
 
-VERSION = os.environ.get('MTA_VERSION', '1.4.0-2')
-
+VERSION = os.environ.get('MTA_VERSION', '1.4.0')
 
 logger = LoggingUtil.init_logging(
     __name__,
