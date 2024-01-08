f16d391b0c41ccf6e48e61641270ac420e06f461
richardbruskiewich <richard.bruskiewich@delphinai.com>
2024-01-07 18:03:53 -0800

    Troubleshooting main.sh script - realized that problems encountered mostly due to Windoze carriage returns in .env file

--- main.sh
+++ main.sh
@@ -6,7 +6,9 @@ source .env
 set +a
 
 if [ "$MODE" == "deploy" ]; then
+    echo "Deploying to gunicorn ${WEB_HOST}:${WEB_PORT}"
     gunicorn mta.services.server:APP -b ${WEB_HOST}:${WEB_PORT} -w 4 -k uvicorn.workers.UvicornWorker
 else
+    echo "Running uvicorn APP with --host ${MODE} --port ${WEB_PORT}"
     uvicorn mta.services.server:APP --host ${WEB_HOST} --port ${WEB_PORT} --reload
 fi
